14b3a4ce28bdb97b983c1c7999e9f28cf05ce272
mnalley <mnalley95@gmail.com>
2024-12-18 10:40:02 -0500

    solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+  titles = [];
 
+  arr.forEach((book) => {
+    titles.push(book.title);
+  });
+
+  return titles;
 };
 
 // Do not edit below this line
