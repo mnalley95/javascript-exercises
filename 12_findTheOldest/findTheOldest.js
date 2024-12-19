f1c50044b872a173511386b4d8171cea51616c73
mnalley <mnalley95@gmail.com>
2024-12-19 09:10:03 -0500

    solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, currentPerson) => {
+    if (!currentPerson.yearOfDeath) {
+      currentPerson.yearOfDeath = new Date().getFullYear();
+    }
+    if (!oldest.yearOfDeath) {
+      oldest.yearOfDeath = new Date().getFullYear();
+    }
 
+    oldest =
+      currentPerson.yearOfDeath - currentPerson.yearOfBirth >
+      oldest.yearOfDeath - oldest.yearOfBirth
+        ? currentPerson
+        : oldest;
+    console.log(oldest);
+    return oldest;
+  });
 };
 
 // Do not edit below this line
